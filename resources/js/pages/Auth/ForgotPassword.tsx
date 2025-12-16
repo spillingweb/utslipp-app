a4f5d268303bc29298e4a42432325c34c2731b91
spillingweb <khspilling@gmail.com>
2025-12-15 20:19:35 -0500

    fixed file name errors

---
+++ resources/js/pages/Auth/ForgotPassword.tsx
@@ -0,0 +1,34 @@
+import InputError from '@/components/InputError';
+import Button from '@/components/ui/Button';
+import FormCard from '@/components/ui/FormCard';
+import { Input } from '@/components/ui/Input';
+import ReturnButton from '@/components/ui/ReturnButton';
+import AuthLayout from '@/layouts/AuthLayout';
+import { Head, useForm } from '@inertiajs/react';
+import { FormEventHandler } from 'react';
+
+export default function ForgotPassword({ status }: { status?: string }) {
+    const { data, setData, post, processing, errors } = useForm<Required<{ email: string }>>({
+        email: '',
+    });
+
+    const submit: FormEventHandler = (e) => {
+        e.preventDefault();
+        post(route('password.email'));
+    };
+
+    return (
+        <AuthLayout status={status}>
+            <Head title="Glemt passord" />
+            <FormCard onSubmit={submit} heading="Glemt passordet ditt?">
+                <fieldset>
+                    <label htmlFor="email">Oppgi e-postadressen din, så får du en lenke for å tilbakestille passordet.</label>
+                    <Input id="email" name="email" autoFocus value={data.email} onChange={(e) => setData('email', e.target.value)} />
+                    <InputError message={errors.email} />
+                </fieldset>
+                <Button disabled={processing}>Send meg en lenke!</Button>
+                <ReturnButton href={route('login')}>Tilbake til innlogging</ReturnButton>
+            </FormCard>
+        </AuthLayout>
+    );
+}
---
+++ resources/js/pages/Auth/ResetPassword.tsx
@@ -0,0 +1,91 @@
+import InputError from '@/components/InputError';
+import Button from '@/components/ui/Button';
+import FormCard from '@/components/ui/FormCard';
+import { Input } from '@/components/ui/Input';
+import AuthLayout from '@/layouts/AuthLayout';
+import { Head, useForm } from '@inertiajs/react';
+import { FormEventHandler } from 'react';
+import styles from './Auth.module.css';
+
+interface ResetPasswordProps {
+    token: string;
+    email: string;
+}
+
+type ResetPasswordForm = {
+    token: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+};
+
+export default function ResetPassword({ token, email }: ResetPasswordProps) {
+    const { data, setData, post, processing, errors, reset } = useForm<Required<ResetPasswordForm>>({
+        token: token,
+        email: email,
+        password: '',
+        password_confirmation: '',
+    });
+
+    const submit: FormEventHandler = (e) => {
+        e.preventDefault();
+        post(route('password.store'), {
+            onFinish: () => reset('password', 'password_confirmation'),
+        });
+    };
+
+    return (
+        <AuthLayout >
+            <Head title="Endre passord" />
+            <FormCard onSubmit={submit} >
+                <fieldset className={styles.loginInput}>
+                    <label className="bold" htmlFor="email">E-post</label>
+
+                    <Input
+                        id="email"
+                        type="email"
+                        name="email"
+                        value={data.email}
+                        autoComplete="username"
+                        onChange={(e) => setData('email', e.target.value)}
+                    />
+
+                    <InputError message={errors.email} />
+                </fieldset>
+
+                <fieldset className={styles.loginInput}>
+                    <label className="bold" htmlFor="password">Passord</label>
+
+                    <Input
+                        id="password"
+                        type="password"
+                        name="password"
+                        value={data.password}
+                        autoComplete="new-password"
+                        autoFocus
+                        onChange={(e) => setData('password', e.target.value)}
+                    />
+
+                    <InputError message={errors.password} />
+                </fieldset>
+
+                <fieldset className={styles.loginInput}>
+                    <label htmlFor="password_confirmation">Bekreft passord</label>
+
+                    <Input
+                        type="password"
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        value={data.password_confirmation}
+                        autoComplete="new-password"
+                        onChange={(e) => setData('password_confirmation', e.target.value)}
+                    />
+
+                    <InputError message={errors.password_confirmation} />
+                </fieldset>
+
+                <Button disabled={processing}>Endre passord</Button>
+            </FormCard>
+        </AuthLayout>
+    );
+}
